05df228113e80e46966653b2f6d3c077a1030c0a
Adrien Vidal <adriensg.vidal@gmail.com>
2020-10-28 16:26:04 +0100

    clean

--- src/App.jsx
+++ src/App.jsx
@@ -17,14 +17,18 @@ const App = () => {
   const [loading, setLoading] = useState(false);
   const [alert, setAlert] = useState(null);
 
-  useEffect(async () => {
+  useEffect(() => {
+    getUsers();
+  }, []);
+
+  const getUsers = async () => {
     setLoading(true);
     const res = await axios.get(
       `https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
     );
     setUsers(res.data);
     setLoading(false);
-  }, []);
+  };
 
   // Search github users
   const searchUsers = async (text) => {
